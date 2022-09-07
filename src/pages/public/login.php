04e4c0f3570a011f0ad57fa7725fbfa43ab37fbd
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2022-09-06 21:40:09 -0300

    initial commit

---
+++ src/pages/public/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/login.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>Login</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="bar"></div>
+        <div class="bar2"></div>
+        <form>
+            <div class="logo">
+                <div class="logo-cont">
+                    <div class="b">
+                        <div class="b1"></div>
+                        <div class="b2"></div>
+                    </div>
+                    <div class="d"></div>
+                </div>
+            </div>
+
+            <div class="oneline">
+                <input placeholder="  Usuário" type="text">
+                <input placeholder="  Senha" type="text">
+            </div>
+
+            <div class="show">
+                <label class="switch">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                </label>
+                <p class="title">Mostrar Senha</p>
+            </div>
+        </form>
+    </main>
+</body>
+
+</html>
