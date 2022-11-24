3b70ed141734cdef6e57d8b3e8639c630f7083f4
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2022-11-24 07:22:16 -0300

    page devs

--- databases/backup/lib/Bd/Adapters/PhpMailerAdapter.php
+++ databases/backup/lib/Bd/Adapters/PhpMailerAdapter.php
@@ -27,4 +27,43 @@ class PhpMailerAdapter
     $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $this->mail->Port = MAIL_PORT;
   }
+
+  public function setFrom($email, $name = null)
+  {
+    if (is_null($name)) {
+      $this->mail->setFrom($email);
+    } else {
+      $this->mail->setFrom($email);
+    }
+  }
+
+  public function addAddress($email, $name)
+  {
+    $this->mail->addAddress($email, $name);
+  }
+
+  public function addAttachment($archivePath, $displayName)
+  {
+    if (is_null($displayName)) {
+      $this->mail->addAttachment($archivePath);
+    } else {
+      $this->mail->addAttachment($archivePath, $displayName);
+    }
+  }
+
+  public function mountContent($subject, $body, $altBody = null)
+  {
+    $this->mail->isHTML(true);
+    $this->mail->Subject = $subject;
+    $this->mail->Body = $body;
+
+    if (is_null($altBody)) {
+      $this->mail->altBody = $altBody;
+    }
+  }
+
+  public function send()
+  {
+    $this->mail->send();
+  }
 }
--- src/pages/private/page_devs.php
+++ src/pages/private/page_devs.php
@@ -1,8 +1,422 @@
 <main class="cont">
-    <section class="dev-banner">
-    <img src="src/assets/banner-dev.jpg" alt="">
-    <div class="logo">
-        <img src="src/assets/dev-logo.png" alt="">
-    </div>
+    <section class="devs">
+        <header>
+            <img src="src/assets/banner-dev.jpg" alt="">
+            <div class="logo">
+                <img src="src/assets/dev-logo.png" alt="">
+            </div>
+        </header>
+        <main>
+            <h1>3° Informática - 2020 / 2022</h1>
+            <div class="sobre-nos">
+                <h2>Sobre a Turma</h2>
+                <p>A turma do <mark>3º Informática</mark> nasce com desejo de mudança. Em 2020, a turma se concretizou graças a <mark>EEEP Francisca Neilyta Carneiro Albuquerque</mark>. A busca por caminhos naturais e pela conquista de sonhos futuros é a sede desses estudantes desde o início da caminhada escolar e, com isso, o desejado diploma de ensino médio e do curso técnico vieram apenas como consequência de tanto <mark>aprendizado</mark>, <mark>companheirismo</mark>, <mark>valores</mark> e <mark>empatia</mark> com os colegas de classe. Eles tiveraram a brilhante ideia de programar um <mark>sistema</mark> para a <mark>biblioteca</mark> da escola, ideia que já tinha sido contretizada anos passados, porém, o sistema não foi comcnluído com sucesso. Nesse contexto, a turma está empenhada para realizar com êxito a entrega do sistema da <mark>Biblioteca Digital</mark> da escola de ensino de tempo integral.</p>
+            </div>
+            <div class="alunos-devs">
+                <h2>Alunos</h2>
+                <div class="alunos-devs-cont">
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Alysson Bernardo Carvalho</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Terceiro card-->
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Ana Kecia da Silva</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Quarto card-->
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>André Ribeiro Mendes</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Quinto card-->
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Antônia Cintia Nascimento Queirox</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Sexto card-->
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Antônia Marilane Misquita de Sousa</h1>
+                        <div class="content"></div>
+                    </div>
+                    <!-- Sétimo card-->
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Antônio Janilson de Souza Carvalho</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Oitavo card-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Antônio Ronaldo Oliveira</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Nono card-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Caroline Rodrigues Siqueira</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Décimo card-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Crislano Silva Justino</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Décimo primeiro-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Daniel Ripardo Silva</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Décimo segundo -->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Danilo Victor Pacheco de Lima</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Décimo terceiro-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Débora Patricy Ferreira Sousa</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Décimo quarto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Erica Mirele Marques Sampaio</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Décimo quinto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Francisco Andyl David Caetano</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Décimo sexto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Francisco Darlan Lima Marques</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Décimo sétimo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Gustavo Brito do Nascimento</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Décimo oitavo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>João Márcio Ferreira Lopes</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Décimo nono-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>José Vanderlei Furtuna Tomé</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Vigésimo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Josilene de Oliveira Paulo</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Vigésimo primeiro-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Lara Stefanny Tomaz Menezes</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Vigésimo segundo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Lidiele Carolino Marques</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Vigésimo terceiro-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Lorrana Quele Alvez Silva</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Vigésimo quarto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Lucas do Nascimento de Oliveira</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Vigésimo quinto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Marcelino Marques da Silva</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Vigésimo sexto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Maria Bianca Sousa Prestes</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Vigésimo sétimo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Maria Clara Luiz Fontelene</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Vigésimo oitavo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Maria Clarisse Nascimento Franco</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Vigésimo nono-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Maria Luciana Morais da Silva</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Maria Renata da Ponte Alves</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo primeiro-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Mario André Araújo Albuquerque</h1>
+                        <div class="content"></div>
+                    </div>
+                    <!--Trigésimo segundo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Maycon Marcelino do Nascimento</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!-- Trigésimo terceiro-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Melissa Marques André</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo quarto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Mirele Ricardo Andrade</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo quinto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Natanha Gomes Fidelis</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo sexto-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Pedro Henrique Mariano Barbosa</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo sétimo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Rennan Victor Eduardo Oliveira</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo oitavo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Thais Hellen de Sousa Mendes</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Trigésimo nono-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Victória Ellen Paixão dos Santos</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Quadragésimo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Antônia Marilane Misquita de Sousa</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Quadragésimo primeiro-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Wellington de Sousa Farias</h1>
+                        <div class="content"></div>
+                    </div>
+
+                    <!--Quadragésimo segundo-->
+
+                    <div class="alunos-card">
+                        <div class="img">
+                            <img src="logo" />
+                        </div>
+                        <h1>Willian de Souza Mendes</h1>
+                        <div class="content"></div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </section>
-<main>
+    <main>
